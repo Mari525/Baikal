0575aaedd11bf619056d4be99222c61f6e1bdad9
Mari525 <56230146+Mari525@users.noreply.github.com>
2023-03-17 12:24:55 +0300

    done

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+let burgerOpen = document.getElementById('burger-open');
+let burgerClose = document.getElementById('burger-close');
+let nav = 
+
+burgerOpen.addEventListener('click', function() {
+
+})
